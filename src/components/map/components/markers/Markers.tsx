ab961cb2529b8ade0adbb738212e75215841444d
Nikita_dev <nikitasar64@gmail.com>
2023-07-28 19:32:02 +0400

    fix error 404 with react-leaflet default marker icon

--- src/components/map/components/markers/Markers.tsx
+++ src/components/map/components/markers/Markers.tsx
@@ -1,12 +1,20 @@
 import { IWaypoints } from '@interfaces/interfaces';
+import { Icon } from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
 import { FC } from 'react';
 import { Marker } from 'react-leaflet';
+
+const defaultMarker = new Icon({
+	iconUrl: icon,
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+});
 
 export const Markers: FC<Pick<IWaypoints, 'waypoints'>> = ({ waypoints }) => {
 	return (
 		<>
 			{waypoints.map((waypoint, index) => (
-				<Marker key={index} position={waypoint} />
+				<Marker key={index} position={waypoint} icon={defaultMarker} />
 			))}
 		</>
 	);
